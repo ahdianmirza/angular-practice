0ec56dec430af9f45629594a258e913321ad9338
ahdianmirza <ahdianmirza@gmail.com>
2024-03-13 10:15:34 +0700

    parameterize pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,27 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title: string = 'Angular 15 App';
-  
-  studentInfo: any[] = [];
 
-  constructor() {
-    this.studentInfo = [
-      {
-        name: 'name 1',
-        age: 20,
-        dob: '12/11/2024',
-      },
-      {
-        name: 'name 2',
-        age: 21,
-        dob: '09/11/2024',
-      },
-      {
-        name: 'name 3',
-        age: 22,
-        dob: '01/10/2024',
-      },
-    ];
-  }
+  dob = new Date('01/05/1990');
+
+  salary = 1000000;
+
+  constructor() {}
 }
 
