88ca09c48f90038dc83bcc0e0afb9ead9e11fd37
ahdianmirza <ahdianmirza@gmail.com>
2024-03-14 13:34:40 +0700

    How to Pass Fragments to Route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Angular15Service } from './angular15.service';
 import { Observable, from, of } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -12,25 +13,12 @@ export class AppComponent {
   displayMessage: string = "";
   data: any;
 
-  constructor(private _service: Angular15Service) {}
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit() {
-    // let response = this.getDataWithPromise();
-    // response.then(resolve => {
-    //   console.info(resolve);
-    // }, reject => {
-    //   console.info(reject);
-    // });
-
-    // Observable call
-    // this.getDataWithObservable();
-    // this.data.subscribe((res: any) => {
-    //   console.info(res);
-    // }, (error: any) => {
-    //   alert(error.message);
-    // }, () => {
-    //   alert("Observable completing method");
-    // });
+    this.activatedRoute.fragment.subscribe(section => {
+      this.goToSection(section);
+    })
   }
 
   public getDataWithPromise() {
@@ -46,40 +34,8 @@ export class AppComponent {
     return promise;
   }
 
-  public getDataWithObservable() {
-    // Observable constructor
-    // this.data = new Observable(res => {
-    //   console.info("Observable is strarted");
-
-    //   setTimeout(() => res.next("1"), 1000);
-    //   setTimeout(() => res.next('2'), 2000);
-    //   setTimeout(() => res.next('3'), 3000);
-    //   setTimeout(() => res.error(new Error('Error occured while fetching the data')), 3000);
-    //   setTimeout(() => res.next('4'), 4000);
-    //   setTimeout(() => res.next('5'), 5000);
-    //   setTimeout(() => res.complete(), 5000);
-    // });
-
-    // Using create method
-    // this.data = Observable.create((res: any) => {
-    //   setTimeout(() => res.next("1"), 1000);
-    //   setTimeout(() => res.next('2'), 2000);
-    //   setTimeout(() => res.next('3'), 3000);
-    //   setTimeout(
-    //     () => res.error(new Error('Error occured while fetching the data')),
-    //     3000
-    //   );
-    //   setTimeout(() => res.next('4'), 4000);
-    //   setTimeout(() => res.next('5'), 5000);
-    //   setTimeout(() => res.complete(), 3000);
-    // });
-
-    // const array1: any[] = [1, 2, 3, 4];
-    // const array2: any[] = [8, 9, 7, 1];
-
-    // this.data = of(array1, array2, 5, 6, "Ahdian Mirza");
-
-    // this.data = from(array1);
+  goToSection(section: any = "") {
+    document.getElementById(section)!.scrollIntoView({behavior: 'smooth'});
   }
 }
 
