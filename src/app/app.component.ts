c2fa1235748fad0af31fa5e084e064b0fcfa4413
ahdianmirza <ahdianmirza@gmail.com>
2024-03-14 09:40:28 +0700

    different ways to create observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Angular15Service } from './angular15.service';
-import { Observable } from 'rxjs';
+import { Observable, from, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -35,7 +35,7 @@ export class AppComponent {
 
   public getDataWithPromise() {
     let promise = new Promise((resolve, reject) => {
-      const val = 2+1;
+      const val = 2+2;
       if (val == 4) {
         resolve("Success");
       } else {
@@ -47,17 +47,39 @@ export class AppComponent {
   }
 
   public getDataWithObservable() {
-    this.data = new Observable(res => {
-      console.info("Observable is strarted");
+    // Observable constructor
+    // this.data = new Observable(res => {
+    //   console.info("Observable is strarted");
 
-      setTimeout(() => res.next("1"), 1000)
-      setTimeout(() => res.next('2'), 2000);
-      setTimeout(() => res.next('3'), 3000);
-      // setTimeout(() => res.error(new Error('Error occured while fetching the data')), 3000);
-      setTimeout(() => res.next('4'), 4000);
-      setTimeout(() => res.next('5'), 5000);
-      setTimeout(() => res.complete(), 5000);
-    });
+    //   setTimeout(() => res.next("1"), 1000);
+    //   setTimeout(() => res.next('2'), 2000);
+    //   setTimeout(() => res.next('3'), 3000);
+    //   setTimeout(() => res.error(new Error('Error occured while fetching the data')), 3000);
+    //   setTimeout(() => res.next('4'), 4000);
+    //   setTimeout(() => res.next('5'), 5000);
+    //   setTimeout(() => res.complete(), 5000);
+    // });
+
+    // Using create method
+    // this.data = Observable.create((res: any) => {
+    //   setTimeout(() => res.next("1"), 1000);
+    //   setTimeout(() => res.next('2'), 2000);
+    //   setTimeout(() => res.next('3'), 3000);
+    //   setTimeout(
+    //     () => res.error(new Error('Error occured while fetching the data')),
+    //     3000
+    //   );
+    //   setTimeout(() => res.next('4'), 4000);
+    //   setTimeout(() => res.next('5'), 5000);
+    //   setTimeout(() => res.complete(), 3000);
+    // });
+
+    const array1: any[] = [1, 2, 3, 4];
+    const array2: any[] = [8, 9, 7, 1];
+
+    // this.data = of(array1, array2, 5, 6, "Ahdian Mirza");
+
+    this.data = from(array1);
   }
 }
 
